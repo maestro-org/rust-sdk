07be8052f5a9eba5ccb14b753c463af83f40bd42
RJ Regenold <rj@book.io>
2024-03-12 16:55:59 -0500

    updates types to match api spec.

--- src/models/epochs.rs
+++ src/models/epochs.rs
@@ -5,8 +5,8 @@ use serde::Deserialize;
 pub struct Epoch {
     pub blk_count: i32,
     pub epoch_no: i32,
-    pub fees: i32,
-    pub start_time: i32,
+    pub fees: String,
+    pub start_time: i64,
     pub tx_count: i32,
 }
 
